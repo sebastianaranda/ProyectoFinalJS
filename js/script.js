5d2bf35a2e5bf47fac69e9ae0a72eb0f09e2f205
Sebastian Aranda <sebastian.aranda.12@gmail.com>
2021-06-17 14:27:26 -0300

    Finalizo estructura de index y mejoro el css

--- js/script.js
+++ js/script.js
@@ -30,7 +30,7 @@ function cargarCarrito(cantVendida, productoVendido) {
 function menuCompra() {
     seleccionDeMenu = 9;
     while (seleccionDeMenu != 0) {
-        seleccionDeMenu = Number(prompt(`Ingrese el número ID del producto que desea seleccionar: 1-${figuraColeccionable.nombre} / 2-${remeraEstampada.nombre} / 9-Checkout / 0-Salir`));
+        seleccionDeMenu = Number(prompt(`Ingrese el número ID del producto que desea seleccionar:\n\n1 - ${figuraColeccionable.nombre} \n\n2 - ${remeraEstampada.nombre}\n\n9 - Checkout\n\n0 - Salir\n\n`));
         switch (seleccionDeMenu) {
             case 1:
                 venderProducto(figuraColeccionable);
@@ -56,14 +56,14 @@ function totalCarrito() {
 }
 
 function venderProducto(producto) {
-    let cant = Number(prompt(`El valor por unidad es $${producto.precio}. Ingrese la cantidad que desea adquirir: `));
+    let cant = Number(prompt(`El valor por unidad es $${producto.precio}.\n\nIngrese la cantidad que desea adquirir:\n\n`));
     if (producto.vender(cant)) {
         cargarCarrito(cant, producto);
         console.log(`Valor de carrito actualizado $${totalCarrito()}`);
         console.table(carrito);
         alert(`Se agregaron ${cant} unidades de ${producto.nombre} al carrito, por un valor de: $${producto.precio * cant}. `);
     } else {
-        alert(`Lamentablemente no hay suficiente stock. ${producto.nombre} en stock: ${producto.stock}.`);
+        alert(`Lamentablemente no hay suficiente stock.\n\n${producto.nombre} en stock: ${producto.stock}.`);
     }
 }
 
@@ -78,11 +78,11 @@ function checkout() {
         let totalDeCompra = totalCarrito();
         let opcionCheckout = 0;
         while ((opcionCheckout != 1) && (opcionCheckout != 9)) {
-            opcionCheckout = Number(prompt(`Su carrito tiene un valor de: $${totalDeCompra}. Ingrese 1 para confirmar la compra o 9 para seguir comprando.`));
+            opcionCheckout = Number(prompt(`Su carrito tiene un valor de: $${totalDeCompra}. Ingrese una opción:\n\n1 - Confirmar compra\n\n9 - Seguir comprando\n\n`));
             if (opcionCheckout == 1) {
                 console.log("El usuario compro los siguientes productos:")
                 console.table(carrito);
-                alert(`Los $${totalDeCompra} ya fueron debitados de su cuenta. ¡Muchas gracias por su compra!`);
+                alert(`Los $${totalDeCompra} ya fueron debitados de su cuenta.\n\n¡Muchas gracias por su compra!`);
                 vaciarCarrito(carrito);
             }
         }
