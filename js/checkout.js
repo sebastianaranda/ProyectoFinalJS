ebb26eadf3248550ad4d37f656c99f65e3c2b7ff
Sebastian Aranda <sebastian.aranda.12@gmail.com>
2021-07-22 01:51:43 -0300

    Agrego logica de checkout y sweetalert de confirmacion

--- js/checkout.js
+++ js/checkout.js
@@ -4,6 +4,12 @@ let precioTotal;
 
 /* ---------- Selectores ---------- */
 const listaProductos = document.querySelector("#lista-productos");
+const form = document.querySelector(".needs-validation");
+const precioCompra = document.querySelector("#precio__total");
+
+/* ---------- Listeners ---------- */
+form.addEventListener("submit", validarForm);
+
 
 /* ---------- JQuery para el Storage ---------- */
 $(document).ready(() => {
@@ -21,10 +27,7 @@ function insertarCarritoCheckout() {
 
     carrito.forEach(producto => {
         const { nombre, precio, precioUnidad, id, imagen, cantidad } = producto;
-        /* const row = document.createElement("tr");*/
-        //tablaCarrito.appendChild(row);
         precioTotal = precioTotal + precio;
-        //precioCarrito.textContent = `$${precioTotal}`;
         const divCard = document.createElement("div");
         divCard.classList.add("col");
         divCard.innerHTML = `
@@ -41,5 +44,36 @@ function insertarCarritoCheckout() {
         `;
         listaProductos.appendChild(divCard);
     });
+    precioCompra.textContent = `Total: $${precioTotal}`;
     console.table(carrito);
+}
+
+function validarForm(e) {
+    e.preventDefault();
+    if (!form.checkValidity()) {
+        e.stopPropagation();
+    } else {
+        const swalWithBootstrapButtons = Swal.mixin({
+            customClass: {
+                confirmButton: 'btn btn-success',
+                cancelButton: 'btn btn-danger'
+            },
+            buttonsStyling: false
+        });
+        swalWithBootstrapButtons.fire({
+            title: '¡Gracias por tu compra!',
+            text: "En unos minutos recibirás un email con los detalles de tu pedido.",
+            icon: 'success',
+            confirmButtonText: 'LISTO',
+            allowEscapeKey: false,
+            allowOutsideClick: false
+        }).then((result) => {
+            if (result.isConfirmed) {
+                carrito = [];
+                localStorage.setItem("carrito", JSON.stringify(carrito));
+                window.location = "./index.html";
+            };
+        });
+    }
+    form.classList.add('was-validated');
 }
